6f358c49cec51e9b9909578f79f0643500973f55
Drew Verwiel <verwiel.code@gmail.com>
2023-10-17 09:37:41 -0600

    project setup - font and global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { Roboto_Slab } from 'next/font/google'
+
+const roboto_slab = Roboto_Slab({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto_slab.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
